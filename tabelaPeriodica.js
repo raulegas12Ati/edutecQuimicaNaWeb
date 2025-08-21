4d26ff728db8e27ff1d61b04b231ab4d9bd63e7d
EA - Raul Egas Do Prado Sobrinho <raul_egas@educadventista.org>
2025-08-21 15:45:54 -0300

    O elemento almenta de tamanho sem repetir, mas as informações extras não aparecem mais

--- tabelaPeriodica.js
+++ tabelaPeriodica.js
@@ -1,25 +1,32 @@
-function detalesSobreOElemento() {
+function addClassDetales() {
     let divElemento = document.querySelector(".elemento")
     divElemento.classList.add("detales")
+    adicionainformacoes()
 
-    elementos = document.querySelector(".elemento")
+    if (!divElemento.classList.contains("detales")) {
+        let divElemento = document.querySelector(".elemento")
+        divElemento.classList.add("detales")
+        adicionainformacoes()
+    }
+}
 
-    elementos.innerHTML = ""
-    elementos.innerHTML += `
-    <div class="elemento ametais detales" id="H">
-            <div class="numeroAtomico">
-                <p>1</p>
-            </div>
-            <div class="simbuloAtomico">
-                <h1>H</h1>
-            </div>
-            <div class="nomeElemento">
-                <h2>Hidrogênio</h2>
-            </div>
-            <div class="dados">
-                <p>Massa atômica: 1,008u</p>
-                <p>Configuração Eletrônica: 1s²</p>
-            </div>
-        </div>
-    `
+function adicionainformacoes() {
+    const nomeElemento = document.createElement("div")
+        nomeElemento.classList.add("nomeElemento")
+        nomeElemento.innerHTML = "<h2>Hidrogênio</h2>"
+
+        const dados = document.createElement("div")
+        dados.classList.add("dados")
+        dados.innerHTML = `
+            <p>Massa atômica: 1,008u</p>
+            <p>Configuração Eletrônica: 1s²</p>
+        `
+
+        divElemento.appendChild(nomeElemento)
+        divElemento.appendChild(dados)
+}
+
+function removeClassDetales() {
+    let divElemento = document.querySelector(".elemento")
+    divElemento.classList.remove("detales")
 }
