fe4c0a0a2c1a68cebb29847a099dc6c0d40c6393
EA - Raul Egas Do Prado Sobrinho <raul_egas@educadventista.org>
2025-08-21 15:10:10 -0300

    Avanço na tabela, o elemento almenta e com as informações

--- tabelaPeriodica.js
+++ tabelaPeriodica.js
@@ -1,4 +1,25 @@
-function detalesSobreOElemento(){
+function detalesSobreOElemento() {
     let divElemento = document.querySelector(".elemento")
     divElemento.classList.add("detales")
+
+    elementos = document.querySelector(".elemento")
+
+    elementos.innerHTML = ""
+    elementos.innerHTML += `
+    <div class="elemento ametais detales" id="H">
+            <div class="numeroAtomico">
+                <p>1</p>
+            </div>
+            <div class="simbuloAtomico">
+                <h1>H</h1>
+            </div>
+            <div class="nomeElemento">
+                <h2>Hidrogênio</h2>
+            </div>
+            <div class="dados">
+                <p>Massa atômica: 1,008u</p>
+                <p>Configuração Eletrônica: 1s²</p>
+            </div>
+        </div>
+    `
 }
